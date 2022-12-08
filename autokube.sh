40094baa3bb50bcddb8c168293a231ddd0dad32a
CoalNova <CoalNova@protonmail.com>
2022-12-08 12:07:43 -0500

    0.2

--- autokube.sh
+++ autokube.sh
@@ -3,7 +3,7 @@
 # The name of the pod
 autokube_name="autokube"
 # The image used in the pod
-autokube_imge="nginx"
+autokube_imge="nginx:alpine"
 # The port of the pod
 autokube_port="9876"
 # The name of the associated service
@@ -16,8 +16,8 @@ echo "AutoKube, for your automatic Kube needs."
 echo""
 if [ "$1" ==  "-stop" ]
 then
-    kubectl delete pods -l auto
     kubectl delete service -l auto
+    kubectl delete deployments $autokube_name
 elif [ "$1" == "-start" ]
 then
     kube_=$(kubectl get pods -l auto)
@@ -25,8 +25,9 @@ then
     then
         echo "Auto not found, initializing..."
         echo ""
-        kubectl run $autokube_name -l auto=kube --image=$autokube_imge --port=$autokube_port
-        kubectl expose pod $autokube_name -l auto=kube --port=80 --target-port=$autokube_port --name=$autokube_serv_name
+        kubectl create deployment $autokube_name --image=$autokube_imge --port=$autokube_port
+        kubectl
+        kubectl expose deployment $autokube_name -l auto=kube --port=80 --target-port=$autokube_port --name=$autokube_serv_name
     else
         echo "Already running auto, pass [-stop] to shut down auto."
     fi
@@ -35,6 +36,8 @@ else
     kubectl describe pods -l=auto
     echo ""
     kubectl get pods -l=auto -o wide
+    echo ""
+    kubectl get services -l=auto -o wide
     echo -e "\nPass [-start] to start up auto, or [-stop] to stop auto."
 fi
     echo ""
